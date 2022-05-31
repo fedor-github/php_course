d4bddf7ab45000190ffde16ff54bb2dab900b368
Fedor Tretyakov <fedor@softgrad.com>
2022-05-31 14:44:14 +0300

    * fix ex1 in ch3 * https://app.asana.com/0/0/1202342060199460/f

--- exercises/ch3/ex1.php
+++ exercises/ch3/ex1.php
@@ -1,5 +1,11 @@
 <?php
 // #loops
 /**
- * Create a php script that prints 'Hello World!' 1000 times
+ * Create a php script that prints 'Hello World!' 100 times
+ */
+
+
+/**
+ * Then modify your program
+ * Skip lines 34 and 55 (leave them blank) and print 'Congratulations! You're a Winner!' on the line 66.
  */
