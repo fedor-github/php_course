14618665b85a8f6cb8054d99147b1bc6414f726a
Fedor Tretyakov <fedor@softgrad.com>
2022-06-01 18:51:41 +0300

    * ch4 - standard functions * https://app.asana.com/0/1201506894957597/1202322269346662/f

---
+++ lessons/ch4/listing.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once __DIR__ . '/bootstrap.php';
+
+//require __DIR__ . '/greeting.php';
+//require __DIR__ . '/greeting.php';
+//require __DIR__ . '/greeting.php';
+//require __DIR__ . '/greeting.php';
+//
+//echo 'Next code' . PHP_EOL;
+
+$numbers = [];
+for ($i = 1; $i < 101; $i++) {
+    $numbers[] = $i;
+}
+
+for ($j = 0; $j < count($numbers); $j++) {
+    if ($numbers[$j] % 10 === 0) {
+        echo $numbers[$j] . PHP_EOL;
+    }
+}
+
+echo "\n\n";
+
+$number = 25;
+echo "$number % 10 = " . ($number % 10) . PHP_EOL;
+
+$float = 3.14;
+echo "floor($float) = " . floor($float) . PHP_EOL;
+echo "ceil($float) = " . ceil($float) . PHP_EOL;
+echo "round($float, 1) = " . ($res = round($float, 0)) . PHP_EOL;
+printf("round($float, 1) = %f, %s\n", $res, 'John');
+
+echo "\n\n";
+
+$random = [
+    10 => 0,
+    11 => 0,
+    12 => 0,
+    13 => 0,
+    14 => 0,
+    15 => 0,
+    16 => 0,
+    17 => 0,
+    18 => 0,
+    19 => 0,
+    20 => 0,
+];
+for ($i = 0; $i < 1000000; $i++) {
+    $currentRand = rand(10, 20);
+    $random[$currentRand]++;
+}
+
+print_r($random);
+echo "\n";
